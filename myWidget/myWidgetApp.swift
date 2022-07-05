0ee647240e79bf7151dd75dea83e584b7fc59a6d
Techtronis <alexisjost20@gmail.com>
2022-07-05 11:17:35 +0200

    Initial Commit

---
+++ myWidget/myWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  myWidgetApp.swift
+//  myWidget
+//
+//  Created by Alexis Jost on 05.07.22.
+//
+
+import SwiftUI
+
+@main
+struct myWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
